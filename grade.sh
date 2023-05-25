d8d80c4527a83be4d638743e3ced8916f8391840
jhuynh <joh022@ucsd.edu>
2023-05-25 10:17:52 -0700

    push

--- grade.sh
+++ grade.sh
@@ -14,3 +14,21 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [[ -f "student-submission/ListExamples.java" ]];
+then
+    echo "All Files are present"
+else
+    echo "Some Files may be missing"
+fi
+
+scp TestListExamples.java student-submission
+cd student-submission
+mkdir lib
+cd ..
+scp lib/hamcrest-core-1.3.jar student-submission/lib
+scp lib/junit-4.13.2.jar student-submission/lib
+cd student-submission
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
