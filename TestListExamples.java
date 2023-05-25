8557e8e7e5d2029ebabd6275b39d5280bf68f932
jhuynh <joh022@ucsd.edu>
2023-05-25 10:33:50 -0700

    push

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,11 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  // @Test(timeout = 500)
+  // public void testFilter() {
+  //   List<String> input = Arrays.asList("pig", "cat", "chicken");
+  //   List<String> result = ListExamples.filter(sc, input);
+  // }
+
 }
--- grade.sh
+++ grade.sh
@@ -8,23 +8,6 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-if [[ -f "student-submission/ListExamples.java" ]];
-then
-    echo "All Files are present"
-else
-    echo "Some Files may be missing"
-fi
-
-scp TestListExamples.java student-submission
-cd student-submission
-mkdir lib
-cd ..
-scp lib/hamcrest-core-1.3.jar student-submission/lib
-scp lib/junit-4.13.2.jar student-submission/lib
-cd student-submission
-
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
@@ -37,6 +20,7 @@ then
     echo "All Files are present"
 else
     echo "Some Files may be missing"
+    exit 0
 fi
 
 scp TestListExamples.java student-submission
